6caad5c07ba48d2d73e50d379bf5459ee766d4dc
sudhir25464 <sudhir202118@gmail.com>
2024-04-29 10:00:33 +0530

    Basic Operations on JavaScript Arrays

---
+++ VariousOperationOnArray.js
@@ -0,0 +1,76 @@
+
+// Various Basic operations on JavaScript Array
+
+ 
+ let Data = new Array(10, 20, 30, 40, 50, 60);
+ // access first elemet of array
+ console.log(Data[0]);
+ 
+ // Access 3rd element of array
+ console.log(Data[2]);
+ 
+ // Access 5th element of array
+ console.log(Data[4]);
+
+  // Access Last elemet of array
+  console.log(Data[Data.length - 1]);
+
+  // access complete  array
+
+  console.log(Data);
+ 
+  // modify in Array
+  Data[0] = 100;
+  console.log(Data);
+
+//   Adding Elements to the Array
+
+//   Elements can be added to the array using methods like push() and unshift().
+ let courses = ['BCA', 'B.TECH', 'MCA', 'MBA'];
+ console.log(courses);
+// Push method add elements to the  array in  Last
+courses.push('M.tech');
+console.log(courses);
+
+// Unshift method add elements to the  array in  First
+courses.unshift('Business Program');
+console.log(courses);
+
+// Removing Elements from an Array
+// Remove elements using methods like pop(), shift(), or splice().
+// pop() : remove last element from array
+// shift() : remove first element from array
+// splice() : remove multiple elements  according to range
+let number = [1,2, 3,4, 5,6,7,8,9,10];
+console.log(number);
+
+let removeLastElement= number.pop();
+console.log(number);
+
+let RemoveFirstElement = number.shift();
+console.log(number);
+
+let numbers = [1,2, 3,4, 5,6,7,8,9,10];  
+console.log("Org Array is  \n " + numbers);
+
+let removeMutlipeElements = numbers.splice(0,3);
+console.log(numbers)
+let removeMutlipeElements1 = numbers.splice(0,5);
+console.log(numbers)
+
+// get length of Array
+let num= [1,2, 3,4, 5,6,7,8,9,10];
+let len = num.length;
+console.log(len);
+// decrease length of array
+num.length = 7;
+console.log(num.length);
+console.log(num);
+
+num.length = 12;
+console.log(num);
+
+
+
+
+

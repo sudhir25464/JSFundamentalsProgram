71cd313440dcbe4449b99cdf92147420c3f42f06
sudhir25464 <sudhir202118@gmail.com>
2024-04-29 08:15:58 +0530

    find Greatest

---
+++ FindGreatest.js
@@ -0,0 +1,12 @@
+
+// Find Greatest 
+
+const  greater =(x,y) =>{
+    if(x > y ){
+        return " x is greater than y value is " +x 
+    }else{
+        return "y is greater than x  value is " +y
+    }
+
+}
+console.log(greater(67,89));

14226d8cef037647d76bdddfc02bedf756da883a
sudhir25464 <sudhir202118@gmail.com>
2024-04-29 08:04:57 +0530

    Find factorial

---
+++ Factorial.js
@@ -0,0 +1,13 @@
+
+
+// find factorial using arrow function with argument
+
+const factorial = (n) => {
+    if(n == 0){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+console.log("Factorial is  "+factorial(5));

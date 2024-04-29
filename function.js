3a817ec25e09bd204cd648308e0d81c6b949a0c9
sudhir25464 <sudhir202118@gmail.com>
2024-04-29 07:59:28 +0530

    function program

---
+++ function.js
@@ -0,0 +1,18 @@
+
+// arrow function in js 
+// write a program find addtion of two number in js
+
+const sum = (a,b) => (a+b)
+
+console.log(sum(10,20));
+
+// In javaScript here we can write function in different ways
+
+// Second way
+// normal function
+function sum1(a,b){
+    console.log(a+b);
+
+}
+sum1(5,10); // call  function 
+

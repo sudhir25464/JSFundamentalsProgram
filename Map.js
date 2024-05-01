a238a2b701f09deafefa6c9b277a22cc013e811d
sudhir25464 <sudhir202118@gmail.com>
2024-05-01 09:50:43 +0530

    JavaScript Map

---
+++ Map.js
@@ -0,0 +1,26 @@
+
+// create  map
+
+let table = new Map ([
+    [1,2],
+    [2,4],
+    [4,6]
+]);
+
+let ProductRate = new Map([
+    ["Laptop", 25000],
+    ["Desktop", 30000],
+    ["Watches", 2000]
+]);
+
+// set method used to add key with value in js
+ProductRate.set('Phone', 1000);
+
+
+
+// print map
+console.log(table);
+console.log(ProductRate);
+
+let set1 = new Set(["sumit","sumit","amit","amit","anil","anish"]);
+console.log(set1);

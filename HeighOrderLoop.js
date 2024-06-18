7051fc13c507bf69161e46046c80279bcf2796e4
sudhir25464 <sudhir202118@gmail.com>
2024-06-18 16:48:27 +0530

    upgrate concept of for..in , for..of and forEach Loop

---
+++ HeighOrderLoop.js
@@ -0,0 +1,79 @@
+const str = "sudhir singh";
+
+for(const latter of str){
+    
+    console.log(`my name ${latter}`);
+}
+
+
+// map
+
+const map = new Map();
+
+map.set('IN', 'INDIA');
+map.set('OUT', 'OURIE');
+map.set('IN', ' INDIA');
+map.set('OUT', 'OSFR');
+
+// console.log(map);
+
+for(const value of map){ // IT rerurn result in the form of aaray
+    console.log(value);
+}
+
+for(const [key,value] of map){ //destructure of array
+    console.log(key,' - ', value);
+}
+
+
+// FOR IN LOOP ON 0ITERABLE OBJECT
+
+// const ar = [12,32,32,4,2];
+
+// const string=['dnsj','js','cpp','json'];  it return 0,1,2,3
+
+// for (const key in string){
+//     console.log(key);
+// }
+
+
+const myobject ={
+    name:'sudhir',
+    age:25,
+    city:'banglore'
+}
+
+for(const key in myobject){
+
+console.log(`${key} :- ${myobject[key]}`);
+}
+
+// FOREACH
+
+const programming = ['js','HTML','CSS','JAVA', 'RRACT'];
+
+
+programming.forEach((item,index)=>{
+    console.log(item, index);
+})
+
+// declare array of objects
+
+const myCoding = [
+    {
+        languages:'javascript',
+        languagesfilename:'js',
+    },
+    {
+        languages:'java',
+        languagesfilename:'java',
+    },
+    {
+        languages:'python',
+        languagesfilename:'py',
+    }
+]
+
+myCoding.forEach((item,index, myCoding)=>{
+    console.log(item.languages, index,);
+})
--- IIFE.js
+++ IIFE.js
@@ -1,3 +1,7 @@
+//
+// An IIFE (Immediately Invoked Function Expression) in JavaScript is a function that is defined and executed immediately after its creation. This is done to create a new scope and avoid polluting the global namespace with variables. IIFEs are useful for encapsulating code and creating
+//  module-like structures before the advent of ES6 modules.
+
 (function output(){
     console.log("Hello World");
 })();

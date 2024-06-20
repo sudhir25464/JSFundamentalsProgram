d570ea7b72c4bbaefc148fe6043b61aed9a818d1
sudhir25464 <sudhir202118@gmail.com>
2024-06-20 18:50:43 +0530

    Today recap concept of clouser in js

---
+++ Closer.js
@@ -0,0 +1,26 @@
+// What is a Closure?
+// A closure is a function that retains access to its lexical scope,
+//  meaning it can remember and access variables from its outer scope even after the outer function has completed execution.
+
+// How Does it Work?
+// When a function is defined inside another function, it forms a closure. The inner function maintains a reference to the outer function’s variables, creating a persistent scope chain. This allows the inner function to access and manipulate the outer 
+// function's variables even after the outer function has returned.
+
+
+function sum(){
+
+     let sum = 0 ;
+    
+    console.log('this outer function');
+     return function totalsum (val1, val2){
+        sum  = val1 +val2;
+        return sum;
+     }
+   
+    
+
+}
+
+const result = sum();
+console.log(result(1,5));
+// console.log(sum());
